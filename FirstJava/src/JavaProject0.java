5a8075046dd1313d68e2bf0dd95e5ecb46b99a47
javaisnotmydad <smoothiefrog@gmail.com>
2017-09-05 19:58:00 +0800

    ccj -> hdh

--- FirstJava/src/JavaProject0.java
+++ FirstJava/src/JavaProject0.java
@@ -3,7 +3,7 @@ public class JavaProject0 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ccj pashige ruozhi ba");
+		System.out.println("hdh pashige ruozhi ba");
 	}
 
 }
